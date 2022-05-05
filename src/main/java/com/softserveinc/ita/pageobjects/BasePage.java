b58feab3b8bf82c1cc669d6daa3712a738b63650
Nazarii Kupchenko <nazarii.kupchenko@gmail.com>
2022-05-05 18:01:21 +0300

    updated base page

--- src/main/java/com/softserveinc/ita/pageobjects/BasePage.java
+++ src/main/java/com/softserveinc/ita/pageobjects/BasePage.java
@@ -2,6 +2,7 @@ package com.softserveinc.ita.pageobjects;
 
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 
 public class BasePage {
 
@@ -9,6 +10,7 @@ public class BasePage {
 
   public BasePage(WebDriver driver) {
     this.driver = driver;
+    PageFactory.initElements(driver, this);
   }
 
   public void implicitWait(long timeToWait){
