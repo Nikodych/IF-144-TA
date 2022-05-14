0cacfd4e77efc5ceb1fb5b3f29787635fce463d2
Nazarii Kupchenko <nazarii.kupchenko@gmail.com>
2022-05-14 10:48:14 +0300

    add config properties

--- src/main/java/com/softserveinc/ita/utils/TestRunner.java
+++ src/main/java/com/softserveinc/ita/utils/TestRunner.java
@@ -1,31 +1,29 @@
 package com.softserveinc.ita.utils;
 
-
-
-import static com.softserveinc.ita.utils.Properties.BASE_URL;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 import com.softserveinc.ita.pageobjects.GooglePage;
 import com.softserveinc.ita.pageobjects.RozetkaPage;
 import com.softserveinc.ita.pageobjects.RozetkaSearchResultsPage;
 import com.softserveinc.ita.pageobjects.SearchResultsPage;
+import java.io.IOException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
-import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class TestRunner {
 
   private WebDriver driver;
 
   @BeforeMethod
-  public void setUp() {
+  public void setUp() throws IOException {
+    ConfigReader.readProp();
     System.setProperty("webdriver.chrome.driver", "src/main/resources/driver/chromedriver");
     driver = new ChromeDriver();
     driver.manage().window().maximize();
     driver.manage().timeouts().implicitlyWait(60, SECONDS);
-    driver.get(BASE_URL);
+    driver.get(ConfigReader.prop.getProperty("base.url"));
   }
 
   @AfterMethod
