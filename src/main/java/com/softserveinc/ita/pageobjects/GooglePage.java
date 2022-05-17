6b91f2f74e7c1520df73953e9a0301da60db2cfb
Nazarii Kupchenko <nazarii.kupchenko@gmail.com>
2022-05-17 13:23:05 +0300

    update method on google page

--- src/main/java/com/softserveinc/ita/pageobjects/GooglePage.java
+++ src/main/java/com/softserveinc/ita/pageobjects/GooglePage.java
@@ -4,14 +4,15 @@ import static com.codeborne.selenide.Selenide.$;
 
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 
 public class GooglePage {
 
   private final SelenideElement searchField = $(By.name("q"));
 
-  public void makeSearch(String keyword) {
+  public SearchResultsPage makeSearch(String keyword) {
     searchField.setValue(keyword);
     searchField.pressEnter();
+
+    return new SearchResultsPage();
   }
 }
