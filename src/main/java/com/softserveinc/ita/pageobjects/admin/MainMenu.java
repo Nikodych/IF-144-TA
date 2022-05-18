1f5425f6c31580810d54cc3818075738faff92d2
promahos <a.lukavenko@gmail.com>
2022-05-18 17:13:27 +0300

    changed locator for username button search

--- src/main/java/com/softserveinc/ita/pageobjects/admin/MainMenu.java
+++ src/main/java/com/softserveinc/ita/pageobjects/admin/MainMenu.java
@@ -1,6 +1,7 @@
 package com.softserveinc.ita.pageobjects.admin;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
 import static org.openqa.selenium.By.xpath;
 
 public class MainMenu {
@@ -12,6 +13,6 @@ public class MainMenu {
     }
 
     public String getLoggedUserName() {
-        return $(xpath("//span[text()='d-tester']/following-sibling::button[1]")).getText();
+        return $x("//mat-toolbar/button[1]").getText();
     }
 }
