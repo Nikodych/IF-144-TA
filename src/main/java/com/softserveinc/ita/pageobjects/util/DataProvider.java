0bb5e06bded8377a23034ce06feafcb6b176f66c
Edita Petrus <angrybird1315@gmail.com>
2022-05-26 14:44:27 +0300

    formatted according to style

--- src/main/java/com/softserveinc/ita/pageobjects/util/DataProvider.java
+++ src/main/java/com/softserveinc/ita/pageobjects/util/DataProvider.java
@@ -21,7 +21,7 @@ public interface DataProvider {
     String START_DATE = readConfig().getString("dates.startDate");
     String END_DATE = readConfig().getString("dates.endDate");
     String ERROR_MESSAGE_WRONG_DATE_ORDER = readConfig().getString("messages.error.wrongDateOrder");
-    String FACULTY= readConfig().getString("search_input.faculty");
-    String DESCRIPTION= readConfig().getString("search_input.description");
-    String INVALID= readConfig().getString("search_input.invalid");
+    String FACULTY= readConfig().getString("searchInput.faculty");
+    String DESCRIPTION= readConfig().getString("searchInput.description");
+    String INVALID= readConfig().getString("searchInput.invalid");
 }
