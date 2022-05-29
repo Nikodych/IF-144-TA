14b2bed6093314c5d080c5aa03f744ea9da504b1
Obitotski <andrew.obitotski@gmail.com>
2022-05-30 00:31:21 +0300

    added conf (browserSize) to test runner

--- src/main/java/com/softserveinc/ita/pageobjects/util/TestRunner.java
+++ src/main/java/com/softserveinc/ita/pageobjects/util/TestRunner.java
@@ -1,17 +1,19 @@
 package com.softserveinc.ita.pageobjects.util;
 
-import static com.codeborne.selenide.Configuration.browser;
-import static com.codeborne.selenide.Selenide.open;
-import static com.softserveinc.ita.pageobjects.util.DataProvider.LOGIN_PAGE_URL;
-
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+
+import static com.codeborne.selenide.Configuration.browser;
+import static com.codeborne.selenide.Configuration.browserSize;
+import static com.codeborne.selenide.Selenide.open;
+import static com.softserveinc.ita.pageobjects.util.DataProvider.LOGIN_PAGE_URL;
 
 public abstract class TestRunner {
 
     @BeforeClass
     public void setUp() {
         browser = "chrome";
+        browserSize = "1920x1080";
     }
 
     @BeforeMethod
