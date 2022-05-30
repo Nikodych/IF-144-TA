9b16fb24f06cb22f7f5f86014fd08f59b6722b69
alukavenko <a.lukavenko@gmail.com>
2022-05-31 00:36:30 +0300

    added test to verify speciality delete feature

--- src/main/java/com/softserveinc/ita/pageobjects/admin/SpecialitiesPage.java
+++ src/main/java/com/softserveinc/ita/pageobjects/admin/SpecialitiesPage.java
@@ -1,12 +1,19 @@
 package com.softserveinc.ita.pageobjects.admin;
 
+import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.disappear;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$$x;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class SpecialitiesPage extends MainMenu {
+
+    private SelenideElement buttonNavigationLast = $x("//button[contains(@class,'paginator-navigation-last')]");
+    private SelenideElement buttonNavigationFirst = $x("//button[contains(@class,'paginator-navigation-first')]");
+    private SelenideElement buttonNavigationNext = $x("//button[contains(@class,'paginator-navigation-next')]");
 
     @Step("Speciality page: Added new speciality")
     public SpecialitiesPage addNewSpeciality(String code, String name) {
@@ -20,8 +27,6 @@ public class SpecialitiesPage extends MainMenu {
 
     @Step("Speciality page: Got last speciality code")
     public String getLastSpecialityCode() {
-        var buttonNavigationLast = $x("//button[contains(@class,'paginator-navigation-last')]");
-
         if (buttonNavigationLast.isEnabled()) {
             buttonNavigationLast.click();
         }
@@ -40,10 +45,61 @@ public class SpecialitiesPage extends MainMenu {
         return this;
     }
 
+    @Step("Speciality page: Waited for progress bar to appear")
+    public SpecialitiesPage waitForProgressBarToAppear() {
+        $x("//mat-progress-bar").should(appear);
+
+        return this;
+    }
+
     @Step("Speciality page: Got pop-up message text")
     public String getMessageText() {
         return $x("//simple-snack-bar/span")
                 .should(appear)
                 .getText();
+    }
+
+    @Step("Speciality page: Deleted speciality by code")
+    public SpecialitiesPage deleteSpecialityByCode(String code) {
+        SelenideElement currentRow = findRowByColumnValue(code);
+        currentRow.$x(".//i[contains(@class,'delete')]").click();
+        $x("//button/span[contains(text(),'Підтвердити')]").click();
+
+        return this;
+    }
+
+    private SelenideElement findRowByColumnValue(String searchValue) {
+        SelenideElement currentRow;
+
+        if (buttonNavigationFirst.isEnabled()) {
+            buttonNavigationFirst.click();
+        }
+
+        currentRow = findRowOnCurrentPageByColumnValue(searchValue);
+
+        while (buttonNavigationNext.isEnabled() && currentRow == null) {
+            buttonNavigationNext.click();
+            buttonNavigationNext
+                    .$x(".//div[contains(@class,'round')]/div[@class='mat-ripple-element']")
+                    .should(disappear);
+
+            currentRow = findRowOnCurrentPageByColumnValue(searchValue);
+        }
+
+        return currentRow;
+    }
+
+    private SelenideElement findRowOnCurrentPageByColumnValue(String searchValue) {
+        SelenideElement searchRow = null;
+        var tableRows = $$x("//table//tr//td");
+        if (tableRows
+                .texts()
+                .contains(searchValue)) {
+            searchRow = tableRows
+                    .find(exactText(searchValue))
+                    .parent();
+        }
+
+        return searchRow;
     }
 }
--- src/test/java/com/softserveinc/ita/SpecialitiesTest.java
+++ src/test/java/com/softserveinc/ita/SpecialitiesTest.java
@@ -17,14 +17,14 @@ public class SpecialitiesTest extends TestRunner {
 
     private SpecialitiesPage specialitiesPage;
 
-    @BeforeMethod (groups = {"positive", "negative"})
+    @BeforeMethod(groups = {"positive", "negative"})
     public void openSpecialitiesPage() {
         specialitiesPage = new LoginPage()
                 .login(ADMIN_LOGIN, ADMIN_PASSWORD)
                 .openSpecialitiesPage();
     }
 
-    @Test (groups = "positive")
+    @Test(groups = "positive")
     @Description("Test to verify Specialities page opening")
     public void verifySpecialitiesPageOpening() {
 
@@ -36,7 +36,7 @@ public class SpecialitiesTest extends TestRunner {
                 .isEqualTo(expectedUrl);
     }
 
-    @Test (groups = "positive")
+    @Test(groups = "positive")
     @Description("Test to verify new speciality is added")
     public void verifyAddingNewSpeciality() {
 
@@ -46,6 +46,7 @@ public class SpecialitiesTest extends TestRunner {
 
         var messageText = specialitiesPage
                 .addNewSpeciality(specialityCode, specialityName)
+                .waitForProgressBarToAppear()
                 .getMessageText();
 
         assertThat(messageText)
@@ -60,5 +61,45 @@ public class SpecialitiesTest extends TestRunner {
                 .as("After adding new speciality speciality with added code " +
                         "should be last in the table")
                 .isEqualTo(specialityCode);
+
+        specialitiesPage.deleteSpecialityByCode(specialityCode);
+    }
+
+    @Test(groups = "positive")
+    @Description("Test to verify speciality is deleted")
+    public void verifyDeletingLastSpeciality() {
+
+        var randCode = getRandomNumber(5);
+        var specialityCode = Integer.toString(randCode); // only numbers, no more than 5 symbols
+        var specialityName = "test" + randCode;
+
+        var lastSpecialityCode = specialitiesPage
+                .addNewSpeciality(specialityCode, specialityName)
+                .waitForProgressBarToAppear()
+                .waitForProgressBarToDisappear()
+                .getLastSpecialityCode();
+
+        assertThat(lastSpecialityCode)
+                .as("After adding new speciality speciality with added code " +
+                        "should be last in the table")
+                .isEqualTo(specialityCode);
+
+        var messageText = specialitiesPage
+                .deleteSpecialityByCode(lastSpecialityCode)
+                .waitForProgressBarToAppear()
+                .getMessageText();
+
+        assertThat(messageText)
+                .as("Message after deleting should contain deleted speciality name")
+                .contains(specialityName);
+
+        lastSpecialityCode = specialitiesPage
+                .waitForProgressBarToDisappear()
+                .getLastSpecialityCode();
+
+        assertThat(lastSpecialityCode)
+                .as("After deleting speciality last speciality in the table " +
+                        "should not have deleted code")
+                .isNotEqualTo(specialityCode);
     }
 }
