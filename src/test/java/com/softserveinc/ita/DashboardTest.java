19bb78ffa12b0c777c954cb28bf1d6ca3ecaf87c
Obitotski <andrew.obitotski@gmail.com>
2022-07-04 02:24:01 +0300

    refactored

--- src/test/java/com/softserveinc/ita/DashboardTest.java
+++ src/test/java/com/softserveinc/ita/DashboardTest.java
@@ -1,12 +1,15 @@
 package com.softserveinc.ita;
 
-import com.codeborne.selenide.Selenide;
 import com.softserveinc.ita.pageobjects.LoginPage;
-import com.softserveinc.ita.pageobjects.admin.DashboardPage;
+import com.softserveinc.ita.pageobjects.admin.*;
 import com.softserveinc.ita.util.TestRunner;
 import io.qameta.allure.Description;
+import org.assertj.core.api.SoftAssertions;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 import static com.softserveinc.ita.util.DataProvider.*;
 import static com.softserveinc.ita.util.WindowTabHelper.getCurrentUrl;
@@ -16,14 +19,14 @@ public class DashboardTest extends TestRunner {
 
     private DashboardPage dashboardPage;
 
-    @BeforeMethod (groups = {"positive", "negative"})
+    @BeforeMethod(groups = {"positive", "negative"})
     public void openDashboardPage() {
         dashboardPage = new LoginPage()
                 .login(ADMIN_LOGIN, ADMIN_PASSWORD)
                 .openDashboardPage();
     }
 
-    @Test (groups = "positive")
+    @Test(groups = "positive")
     @Description("Test to verify Dashboard Us page opening")
     public void verifyDashboardPageOpening() {
 
@@ -35,91 +38,61 @@ public class DashboardTest extends TestRunner {
                 .isEqualTo(expectedUrl);
     }
 
-    @Test (groups = "positive")
+    @Test(groups = "positive")
     @Description("Test to verify Dashboard cards buttons work")
     public void verifyDashboardButtonsWork() {
 
-        dashboardPage.goToFacultiesPage();
-
-        var expectedUrl = FACULTIES_PAGE_URL;
-        var currentUrl = getCurrentUrl();
-
         var soft = getSoftAssert();
 
-        soft.assertThat(currentUrl)
-                .as("Page url should be " + expectedUrl)
-                .isEqualTo(expectedUrl);
+        verifyDashboardButton(FacultiesPage.class, FACULTIES_PAGE_URL, soft);
 
-        dashboardPage.openDashboardPage();
-        dashboardPage.goToGroupsPage();
+        verifyDashboardButton(GroupsPage.class, GROUPS_PAGE_URL, soft);
 
-        expectedUrl = GROUPS_PAGE_URL;
-        currentUrl = getCurrentUrl();
+        verifyDashboardButton(SpecialitiesPage.class, SPECIALITIES_PAGE_URL, soft);
 
-        soft.assertThat(currentUrl)
-                .as("Page url should be " + expectedUrl)
-                .isEqualTo(expectedUrl);
+        verifyDashboardButton(SubjectsPage.class,SUBJECTS_PAGE_URL, soft);
 
-        dashboardPage.openDashboardPage();
-        dashboardPage.goToSpecialitiesPage();
+        verifyDashboardButton(StudentsPage.class, GROUPS_PAGE_URL, soft); //button on the students card on the dashboard page redirect us to groups page
 
-        expectedUrl = SPECIALITIES_PAGE_URL;
-        currentUrl = getCurrentUrl();
+        verifyDashboardButton(AdminsPage.class, ADMINS_PAGE_URL, soft);
 
-        soft.assertThat(currentUrl)
-                .as("Page url should be " + expectedUrl)
-                .isEqualTo(expectedUrl);
+        verifyDashboardButton(ResultsPage.class, RESULTS_PAGE_URL, soft);
 
-        dashboardPage.openDashboardPage();
-        dashboardPage.goToSubjectsPage();
-
-        expectedUrl = SUBJECTS_PAGE_URL;
-        currentUrl = getCurrentUrl();
-
-        soft.assertThat(currentUrl)
-                .as("Page url should be " + expectedUrl)
-                .isEqualTo(expectedUrl);
-
-        dashboardPage.openDashboardPage();
-        dashboardPage.goToStudentsPage();
-
-        expectedUrl = GROUPS_PAGE_URL; //button on the students card on the dashboard page redirect us to groups page
-        currentUrl = getCurrentUrl();
-
-        soft.assertThat(currentUrl)
-                .as("Page url should be " + expectedUrl)
-                .isEqualTo(expectedUrl);
-
-        dashboardPage.openDashboardPage();
-        dashboardPage.goToAdminsPage();
-
-        expectedUrl = ADMINS_PAGE_URL;
-        currentUrl = getCurrentUrl();
-
-        soft.assertThat(currentUrl)
-                .as("Page url should be " + expectedUrl)
-                .isEqualTo(expectedUrl);
-
-        dashboardPage.openDashboardPage();
-        dashboardPage.goToResultsPage();
-
-        expectedUrl = RESULTS_PAGE_URL;
-        currentUrl = getCurrentUrl();
-
-        soft.assertThat(currentUrl)
-                .as("Page url should be " + expectedUrl)
-                .isEqualTo(expectedUrl);
-
-        dashboardPage.openDashboardPage();
-        dashboardPage.goToProtocolPage();
-
-        expectedUrl = PROTOCOL_PAGE_URL;
-        currentUrl = getCurrentUrl();
-
-        soft.assertThat(currentUrl)
-                .as("Page url should be " + expectedUrl)
-                .isEqualTo(expectedUrl);
+        verifyDashboardButton(ProtocolPage.class, PROTOCOL_PAGE_URL, soft);
 
         soft.assertAll();
+    }
+
+    private <T> void verifyDashboardButton(Class<T> type, String expectedUrl, SoftAssertions soft) {
+        var nameOfMethod = ("goTo" + type.getSimpleName());
+        Method method = getMethod(nameOfMethod);
+        runVerificationOfDashboardButton(method, expectedUrl, soft);
+    }
+
+    private void runVerificationOfDashboardButton(Method method, String expectedUrl, SoftAssertions soft) {
+        dashboardPage.openDashboardPage();
+        runMethod(dashboardPage, method);
+
+        var currentUrl = getCurrentUrl();
+
+        soft.assertThat(currentUrl)
+                .as("Page url should be " + expectedUrl)
+                .isEqualTo(expectedUrl);
+    }
+
+    private Method getMethod(String name) {
+        try {
+            return DashboardPage.class.getMethod(name);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void runMethod(Object object, Method method) {
+        try {
+            method.invoke(object);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
