63c91e3d0eea26b519ef216f684f5eeb1c1beaf5
Obitotski <andrew.obitotski@gmail.com>
2022-07-03 20:27:11 +0300

    added dashboard buttons test

--- src/main/java/com/softserveinc/ita/pageobjects/admin/DashboardPage.java
+++ src/main/java/com/softserveinc/ita/pageobjects/admin/DashboardPage.java
@@ -1,4 +1,75 @@
 package com.softserveinc.ita.pageobjects.admin;
 
+import com.softserveinc.ita.models.EntitiesButtons;
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Selenide.$x;
+import static com.softserveinc.ita.models.EntitiesButtons.*;
+import static java.lang.String.format;
+import static java.time.Duration.ofSeconds;
+
 public class DashboardPage extends MainMenu<DashboardPage> {
+
+    @Step("Dashboard page: Opened Faculties page")
+    public FacultiesPage goToFacultiesPage() {
+        openPageWithCardButton(FACULTIES_PAGE);
+
+        return new FacultiesPage();
+    }
+
+    @Step("Dashboard page: Opened Groups page")
+    public GroupsPage goToGroupsPage() {
+        openPageWithCardButton(GROUPS_PAGE);
+
+        return new GroupsPage();
+    }
+
+    @Step("Dashboard page: Opened Specialities page")
+    public SpecialitiesPage goToSpecialitiesPage() {
+        openPageWithCardButton(SPECIALITIES_PAGE);
+
+        return new SpecialitiesPage();
+    }
+
+    @Step("Dashboard page: Opened Subjects page")
+    public SubjectsPage goToSubjectsPage() {
+        openPageWithCardButton(SUBJECTS_PAGE);
+
+        return new SubjectsPage();
+    }
+
+    @Step("Dashboard page: Opened Students page")
+    public GroupsPage goToStudentsPage() {
+        openPageWithCardButton(STUDENTS_PAGE);
+
+        return new GroupsPage();
+    }
+
+    @Step("Dashboard page: Opened Admins page")
+    public AdminsPage goToAdminsPage() {
+        openPageWithCardButton(ADMINS_PAGE);
+
+        return new AdminsPage();
+    }
+
+    @Step("Dashboard page: Opened Results page")
+    public ResultsPage goToResultsPage() {
+        openPageWithCardButton(RESULTS_PAGE);
+
+        return new ResultsPage();
+    }
+
+    @Step("Dashboard page: Opened Protocol page")
+    public ProtocolPage goToProtocolPage() {
+        openPageWithCardButton(PROTOCOL_PAGE);
+
+        return new ProtocolPage();
+    }
+
+    private void openPageWithCardButton(EntitiesButtons pageName) {
+        $x(format("//mat-card-title[contains(text(),'%s') or contains(text(),'%s')]/ancestor::mat-card//mat-card-actions/button", pageName.getCardTitleEng(), pageName.getCardTitleUa()))
+                .should(appear, ofSeconds(5))
+                .click();
+    }
 }
--- src/test/java/com/softserveinc/ita/DashboardTest.java
+++ src/test/java/com/softserveinc/ita/DashboardTest.java
@@ -1,5 +1,6 @@
 package com.softserveinc.ita;
 
+import com.codeborne.selenide.Selenide;
 import com.softserveinc.ita.pageobjects.LoginPage;
 import com.softserveinc.ita.pageobjects.admin.DashboardPage;
 import com.softserveinc.ita.util.TestRunner;
@@ -32,5 +33,93 @@ public class DashboardTest extends TestRunner {
         assertThat(currentUrl)
                 .as("Page url should be " + expectedUrl)
                 .isEqualTo(expectedUrl);
+    }
+
+    @Test (groups = "positive")
+    @Description("Test to verify Dashboard cards buttons work")
+    public void verifyDashboardButtonsWork() {
+
+        dashboardPage.goToFacultiesPage();
+
+        var expectedUrl = FACULTIES_PAGE_URL;
+        var currentUrl = getCurrentUrl();
+
+        var soft = getSoftAssert();
+
+        soft.assertThat(currentUrl)
+                .as("Page url should be " + expectedUrl)
+                .isEqualTo(expectedUrl);
+
+        dashboardPage.openDashboardPage();
+        dashboardPage.goToGroupsPage();
+
+        expectedUrl = GROUPS_PAGE_URL;
+        currentUrl = getCurrentUrl();
+
+        soft.assertThat(currentUrl)
+                .as("Page url should be " + expectedUrl)
+                .isEqualTo(expectedUrl);
+
+        dashboardPage.openDashboardPage();
+        dashboardPage.goToSpecialitiesPage();
+
+        expectedUrl = SPECIALITIES_PAGE_URL;
+        currentUrl = getCurrentUrl();
+
+        soft.assertThat(currentUrl)
+                .as("Page url should be " + expectedUrl)
+                .isEqualTo(expectedUrl);
+
+        dashboardPage.openDashboardPage();
+        dashboardPage.goToSubjectsPage();
+
+        expectedUrl = SUBJECTS_PAGE_URL;
+        currentUrl = getCurrentUrl();
+
+        soft.assertThat(currentUrl)
+                .as("Page url should be " + expectedUrl)
+                .isEqualTo(expectedUrl);
+
+        dashboardPage.openDashboardPage();
+        dashboardPage.goToStudentsPage();
+
+        expectedUrl = GROUPS_PAGE_URL; //button on the students card on the dashboard page redirect us to groups page
+        currentUrl = getCurrentUrl();
+
+        soft.assertThat(currentUrl)
+                .as("Page url should be " + expectedUrl)
+                .isEqualTo(expectedUrl);
+
+        dashboardPage.openDashboardPage();
+        dashboardPage.goToAdminsPage();
+
+        expectedUrl = ADMINS_PAGE_URL;
+        currentUrl = getCurrentUrl();
+
+        soft.assertThat(currentUrl)
+                .as("Page url should be " + expectedUrl)
+                .isEqualTo(expectedUrl);
+
+        dashboardPage.openDashboardPage();
+        dashboardPage.goToResultsPage();
+
+        expectedUrl = RESULTS_PAGE_URL;
+        currentUrl = getCurrentUrl();
+
+        soft.assertThat(currentUrl)
+                .as("Page url should be " + expectedUrl)
+                .isEqualTo(expectedUrl);
+
+        dashboardPage.openDashboardPage();
+        dashboardPage.goToProtocolPage();
+
+        expectedUrl = PROTOCOL_PAGE_URL;
+        currentUrl = getCurrentUrl();
+
+        soft.assertThat(currentUrl)
+                .as("Page url should be " + expectedUrl)
+                .isEqualTo(expectedUrl);
+
+        soft.assertAll();
     }
 }
